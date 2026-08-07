8505ce3eed826896c70a7354188fbe20ae928adc
Cstough <32025644+Cstough@users.noreply.github.com>
2026-08-06 23:01:27 -0400

    Began experimenting with simple databinding from a selected feature.
    For now, simply displays attribute names and values in a giant string in the dockpane.
    
    Will move on to something more robust next

--- AttributePanelV2/Dockpane1ViewModel.cs
+++ AttributePanelV2/Dockpane1ViewModel.cs
@@ -4,6 +4,7 @@ using ArcGIS.Core.Geometry;
 using ArcGIS.Desktop.Catalog;
 using ArcGIS.Desktop.Core;
 using ArcGIS.Desktop.Editing;
+using ArcGIS.Desktop.Editing.Attributes;
 using ArcGIS.Desktop.Extensions;
 using ArcGIS.Desktop.Framework;
 using ArcGIS.Desktop.Framework.Contracts;
@@ -12,19 +13,66 @@ using ArcGIS.Desktop.Framework.Threading.Tasks;
 using ArcGIS.Desktop.KnowledgeGraph;
 using ArcGIS.Desktop.Layouts;
 using ArcGIS.Desktop.Mapping;
+using ArcGIS.Desktop.Mapping.Events;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static ArcGIS.Desktop.Internal.Mapping.Controls.FloorFilter.FloorFilterListControlVM;
 
 namespace AttributePanelV2
 {
     internal class Dockpane1ViewModel : DockPane
     {
+        private readonly Inspector _inspector = new Inspector();
         private const string _dockPaneID = "AttributePanelV2_Dockpane1";
+        private string _attributeText = "No selection";
 
-        protected Dockpane1ViewModel() { }
+        public string AttributeText
+        {
+            get => _attributeText;
+            set => SetProperty(ref _attributeText, value);
+        }
+
+        protected Dockpane1ViewModel() 
+        {
+            MapSelectionChangedEvent.Subscribe(OnSelectionChanged);
+        }
+
+        private void OnSelectionChanged(MapSelectionChangedEventArgs args)
+        {
+            QueuedTask.Run(async () =>
+            {
+                if (args.Selection.Count == 0)
+                {
+                    AttributeText = "No selection";
+                    return;
+                }
+
+                AttributeText = "";
+
+                var selection = args.Selection.ToDictionary();
+
+                var firstSelection = selection.First();
+
+                MapMember mapMember = firstSelection.Key;
+                List<long> objectIds = firstSelection.Value;
+
+                if (mapMember is not FeatureLayer featureLayer)
+                    return;
+
+                long oid = objectIds.First();
+
+                await _inspector.LoadAsync(featureLayer, oid);
+
+                foreach(var attribute in _inspector)
+                {
+                    AttributeText += $"{attribute.FieldName} : {attribute.CurrentValue}\n";
+                }
+
+            });
+        }
 
         /// <summary>
         /// Show the DockPane.
@@ -41,7 +89,7 @@ namespace AttributePanelV2
         /// <summary>
         /// Text shown near the top of the DockPane.
         /// </summary>
-        private string _heading = "My DockPane";
+        private string _heading = "Attributes Extended";
         public string Heading
         {
             get => _heading;
